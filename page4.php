bafece86cc93341483890f842aafd6232ebb2eb3
sambandic <sambandic@gmail.com>
2021-09-17 13:49:17 -0500

    Add files via upload

--- page4.php
+++ page4.php
@@ -10,8 +10,7 @@
   <meta name="description" content="">
   
   
-  <title>About</title>
-  <link rel="stylesheet" href="assets/web/assets/mobirise-icons2/mobirise2.css">
+  <title>Page 4</title>
   <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
   <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-grid.min.css">
   <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-reboot.min.css">
@@ -34,19 +33,158 @@
 <link rel="manifest" href="manifest.json">
 <script src="sw-connect.js"></script>
 <meta name="apple-mobile-web-app-capable" content="yes">
-<link rel="apple-touch-startup-image" media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)" href="assets/images/apple-launch-640x1136.png">
-<link rel="apple-touch-startup-image" media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)" href="assets/images/apple-launch-750x1334.png">
-<link rel="apple-touch-startup-image" media="(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)" href="assets/images/apple-launch-1242x2208.png">
-<link rel="apple-touch-startup-image" media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)" href="assets/images/apple-launch-1125x2436.png">
-<link rel="apple-touch-startup-image" media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)" href="assets/images/apple-launch-1536x2048.png">
-<link rel="apple-touch-startup-image" media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)" href="assets/images/apple-launch-1668x2224.png">
-<link rel="apple-touch-startup-image" media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)" href="assets/images/apple-launch-2048x2732.png">
+<link rel="apple-touch-startup-image" media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)" href="%iphone5%">
+<link rel="apple-touch-startup-image" media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)" href="%iphone6,6s,7,8%">
+<link rel="apple-touch-startup-image" media="(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)" href="%iphone6+,6s+,7+,8+%">
+<link rel="apple-touch-startup-image" media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)" href="%iphoneX%">
+<link rel="apple-touch-startup-image" media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)" href="%iPad-Mini,Air%">
+<link rel="apple-touch-startup-image" media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)" href="%iPad-Pro10%">
+<link rel="apple-touch-startup-image" media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)" href="%iPad-Pro12%">
 <meta name="apple-mobile-web-app-status-bar-style" content="default">
 <meta name="apple-mobile-web-app-title" content="Elpidio">
-<link rel="apple-touch-icon" href="apple-touch-icon.png"></head>
+<link rel="apple-touch-icon" href="%apple-touch-icon%"></head>
 <body>
   
-  <section data-bs-version="5.1" class="menu menu3 cid-sIIil3bdKC" once="menu" id="menu3-f">
+  <section data-bs-version="5.1" class="content11 cid-sJigMLy3bu" id="content11-11">
+    
+    <?php
+/*
+   Aktion: Formulardaten als E-Mail versenden.
+   Formular - PHP 5.4+, Zeichenkodierung: UTF-8
+
+   Erstellt mit dem Formular Generator (17.09.2021) - 
+   http://www.nof-tutorials.com/Webbausteine-Formular-Generator/formulargenerator.php
+
+   Bitte testen Sie das Formular ausführlich und
+   beachten Sie die Hinweise im Quelltext!
+*/
+
+// PHP Fehlermeldungen (1 um das Formular zu testen) anzeigen.
+error_reporting(1); // (0/1)
+
+// Weiterleitung - Nach dem absenden des Formulars,
+// gelangt der Benutzer über einen Link auf folgende Seite:
+$Weiterleitung = "index.php";
+
+$name = isset($_POST["name"]) ? strip_tags(trim($_POST["name"])) : ""; // Name
+$email = isset($_POST["email"]) ? strip_tags(trim($_POST["email"])) : ""; // E-Mail
+$betreff = isset($_POST["betreff"]) ? strip_tags(trim($_POST["betreff"])) : ""; // Betreff
+$nachricht = isset($_POST["nachricht"]) ? strip_tags(trim($_POST["nachricht"])) : ""; // Nachricht
+
+// Benutzereingaben überprüfen
+// Die Meldungen müssen hier eventuell angepasst werden.
+$Fehler = ["name"=>"", "email"=>"", "betreff"=>"", "nachricht"=>""];
+if (isset($_POST["submit"])) {
+ $Fehler["name"] = strlen($_POST["name"]) < 1 ? " Bitte füllen Sie dieses Feld aus!" : "";
+ $Fehler["email"] = filter_var($_POST["email"], FILTER_VALIDATE_EMAIL) === false ? " Geben Sie eine gültige E-Mail-Adresse ein!" : "";
+ $Fehler["betreff"] = strlen($_POST["betreff"]) < 1 ? " Bitte füllen Sie dieses Feld aus!" : "";
+ $Fehler["nachricht"] = strlen($_POST["nachricht"]) < 10 ? " Bitte füllen Sie dieses Feld aus (min. 10 Zeichen)!" : "";
+}
+
+// Formular erstellen
+$Formular = "
+<form action='" . $_SERVER["SCRIPT_NAME"] . "' method='post'>
+
+<p>
+ <label> Name:
+<span class='pflichtfeld'>&#10034; " . $Fehler["name"] . "</span><br>
+  <input type='text' name='name' value='" . $name . "' size='35'>
+ </label>
+</p>
+
+<p>
+ <label> E-Mail:
+<span class='pflichtfeld'>&#10034; " . $Fehler["email"] . "</span><br>
+  <input type='email' name='email' value='" . $email . "' size='35'>
+ </label>
+</p>
+
+<p>
+ <label> Betreff:
+<span class='pflichtfeld'>&#10034; " . $Fehler["betreff"] . "</span><br>
+  <input type='text' name='betreff' value='" . $betreff . "' size='45'>
+ </label>
+</p>
+
+<p>
+ <label> Nachricht:
+ <span class='pflichtfeld'>&#10034; " . $Fehler["nachricht"] . "</span><br>
+ <textarea name='nachricht' cols='40' rows='8'>" . $nachricht . "</textarea>
+ </label>
+</p>
+
+<p>
+ <br>
+ <input type='submit' name='submit' value='Formular absenden'>
+</p>
+
+<p>
+ <small>Bitte alle mit <span class='pflichtfeld'>&#10034;</span>
+ markierten Felder ausfüllen.</small>
+</p>
+
+</form>
+";
+
+// Formular abgesendet
+if (isset($_POST["submit"])) {
+
+ // Sind keine Benutzer-Eingabefehler vorhanden
+ if (implode("", $Fehler) == "") {
+
+  // Daten als E-Mail versenden (Vorschlag) - Bitte anpassen!
+
+  // Zeitzone und das aktuelle Datum setzen
+  // http://de3.php.net/manual/de/timezones.europe.php
+  date_default_timezone_set("Europe/Berlin");
+  $Datum = date("d.m.Y H:i");
+
+  // Empfänger E-Mail
+  $Mailto = "name@example.com";
+
+  // Inhalt der E-Mail setzen
+  $Text = "   Gesendet am: $Datum Uhr
+   Name: $name
+   E-Mail: $email
+   Betreff: $betreff
+   Nachricht: $nachricht
+  ";
+
+  // E-Mail versenden
+  mb_internal_encoding("UTF-8");
+  $Betreff = mb_encode_mimeheader($betreff, "UTF-8", "Q");
+  $Kopfzeile = "MIME-Version: 1.0;\nFrom: " . mb_encode_mimeheader($name, "UTF-8", "Q") .
+   "<" . $email . ">" . "\nContent-Type: text/plain; Charset=UTF-8;\n";
+
+  if (mail($Mailto, $Betreff, $Text, $Kopfzeile)) {
+
+   echo "<p>Vielen Dank, die Nachricht wurde versendet.</p>";
+  }
+  else {
+
+   // Wenn die Daten nicht versendet werden konnten,
+   // wird die E-Mail-Adresse für den direkten Kontakt eingeblendet.
+   echo "<p>Beim Senden der Nachricht ist ein Fehler aufgetreten!<br>" . 
+   "Bitte wenden Sie sich direkt an: <a href='mailto:" . $Mailto . "'>" . $Mailto . "</a></p>";
+  }
+
+  // Weiterleitung
+  echo "<p><a href='" . $Weiterleitung . "' target='_top'>Weiter</a></p>";
+ }
+ else {
+
+  // Formular und Benutzer-Eingabefehler ausgeben
+  echo $Formular;
+ }
+}
+else {
+
+ // Formular ausgeben
+ echo $Formular;
+}
+?></section>
+
+<section data-bs-version="5.1" class="menu menu3 cid-sIIil3bdKC" once="menu" id="menu3-z">
     
     <nav class="navbar navbar-dropdown navbar-fixed-top navbar-expand-lg">
         <div class="container">
@@ -86,76 +224,7 @@
     </nav>
 </section>
 
-<section data-bs-version="5.1" class="testimonials1 cid-sIJb9EpN1X" id="testimonials1-s">
-    
-
-    
-    <div class="container">
-        
-        <div class="row align-items-center">
-            <div class="col-12 col-md-6">
-                <div class="image-wrapper">
-                    <img src="assets/images/team2.jpg" alt="">
-                </div>
-            </div>
-            <div class="col-12 col-md">
-                <div class="text-wrapper">
-                    <p class="mbr-text mbr-fonts-style mb-4 display-7">We are (text here) and are proud to serve you!</p>
-                    <p class="name mbr-fonts-style mb-1 display-4">
-                        <strong>Elpidio&nbsp;</strong></p>
-                    <p class="position mbr-fonts-style display-4">
-                        <strong>Owner &amp; CEO</strong></p>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
-
-<section data-bs-version="5.1" class="testimonials2 cid-sIJbdHAnij" id="testimonials2-t">
-    
-    
-    <div class="container">
-        <h3 class="mbr-section-title mbr-fonts-style align-center mb-4 display-2">
-            <strong>Testimonials</strong>
-        </h3>
-        <div class="row justify-content-center">
-            <div class="card col-12 col-md-6">
-                <p class="mbr-text mbr-fonts-style mb-4 display-7">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc consectetur malesuada sem, vel placerat orci pretium eget. Fusce pretium, nulla sit amet vestibulum lacinia, erat odio gravida mi, eu fringilla quam dui nec risus. Vestibulum nisi metus, auctor blandit tincidunt et, consectetur eget turpis. </p>
-                <div class="d-flex mb-md-0 mb-4">
-                    <div class="image-wrapper">
-                        <img src="assets/images/team1.jpg" alt="">
-                    </div>
-                    <div class="text-wrapper">
-                        <p class="name mbr-fonts-style mb-1 display-4">
-                            <strong>Martin Smith</strong>
-                        </p>
-                        <p class="position mbr-fonts-style display-4">
-                            <strong>Client</strong>
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div class="card col-12 col-md-6">
-                <p class="mbr-text mbr-fonts-style mb-4 display-7">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc consectetur malesuada sem, vel placerat orci pretium eget. Fusce pretium, nulla sit amet vestibulum lacinia, erat odio gravida mi, eu fringilla quam dui nec risus. Vestibulum nisi metus, auctor blandit tincidunt et, consectetur eget turpis. </p>
-                <div class="d-flex mb-md-0 mb-4">
-                    <div class="image-wrapper">
-                        <img src="assets/images/team2.jpg" alt="">
-                    </div>
-                    <div class="text-wrapper">
-                        <p class="name mbr-fonts-style mb-1 display-4">
-                            <strong>Jessica Brown</strong>
-                        </p>
-                        <p class="position mbr-fonts-style display-4">
-                            <strong>Client</strong>
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
-
-<section data-bs-version="5.1" class="footer5 cid-sIJ4XoRX1d mbr-reveal" once="footers" id="footer5-m">
+<section data-bs-version="5.1" class="footer5 cid-sIJ4XoRX1d mbr-reveal" once="footers" id="footer5-10">
 
     
 
@@ -163,14 +232,14 @@
 
     <div class="container">
         <div class="media-container-row">
-            <div class="col-md-2 col-6">
+            <div class="col-md-3 col-6">
                 <div class="media-wrap">
                     <a href="#">
                         <img src="assets/images/untitled-8.png" alt="">
                     </a>
                 </div>
             </div>
-            <div class="col-10 col-6">
+            <div class="col-9 col-6">
                 <p class="mbr-text align-right links mbr-fonts-style display-7"><a href="index.html#top" class="text-primary">Home&nbsp;</a> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; <a href="services.html#top" class="text-primary">Services&nbsp;</a> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; <a href="contact.html#contacts2-r" class="text-primary">&nbsp; Contact&nbsp;</a> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<a href="about.html#top" class="text-primary">About Us</a></p>
             </div>
         </div>
